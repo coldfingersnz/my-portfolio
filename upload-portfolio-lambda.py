e495ad142659ebf375e93264c0b237f401641182
coldfingersnz <cookster@orcon.net.nz>
2018-10-11 09:09:21 +1300

    Update portfolio deployment function for lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,20 +1,26 @@
+import json
 import boto3
 import StringIO
 import zipfile
 import mimetypes
+def lambda_handler(event, context):
 
-s3 = boto3.resource('s3')
+    s3 = boto3.resource('s3')
 
-portfolio_bucket = s3.Bucket('portfolio.opaki.net')
-build_bucket = s3.Bucket('portfoliobuild.opaki.net')
+    portfolio_bucket = s3.Bucket('portfolio.opaki.net')
+    build_bucket = s3.Bucket('portfoliobuild.opaki.net')
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj =myzip.open(nm)
-        portfolio_bucket.upload_fileobj(obj, nm,
-        ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
-        
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj =myzip.open(nm)
+            portfolio_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+        # TODO implement
+        return {
+            "statusCode": 200,
+            "body": json.dumps('Hello from Lambda!')
+        }
